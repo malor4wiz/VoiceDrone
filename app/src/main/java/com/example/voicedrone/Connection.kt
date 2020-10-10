1a92969a3676f113397d9216f81be8117aecf6ab
Hidenori Yabuuchi <hidenori111yellow@icloud.com>
2020-10-10 10:14:58 +0900

    first commit

---
+++ app/src/main/java/com/example/voicedrone/Connection.kt
@@ -0,0 +1,82 @@
+package com.example.voicedrone
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiConfiguration
+import android.net.wifi.WifiManager
+import android.os.Build
+import android.util.Log
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.CompletableEmitter
+import io.reactivex.rxjava3.core.Single
+import java.util.concurrent.TimeUnit
+
+class Connection(private val context: Context, private val wifiSSID: String, private val wifiPassword: String) {
+
+    private var wifiManager =
+        context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+    private val connectivityManager =
+        context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    operator fun invoke() {
+        Log.v("v", "invoke")
+        connect()
+    }
+
+    private fun connect() {
+        Log.v("v", "connect")
+        val intentFilter = IntentFilter(WifiManager.NETWORK_STATE_CHANGED_ACTION)
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if (isConnectedToCorrectSSID()) {
+                    Log.v("v","Successfully connected to the device.")
+                } else {
+                    Log.w("w","Still not connected to ${wifiSSID}. Waiting a little bit more...")
+                }
+            }
+        }
+        Log.v("v","Registering connection receiver...")
+        context.registerReceiver(receiver, intentFilter)
+        addNetwork()
+    }
+
+    private fun initNetwork(): Single<Network> {
+        Log.v("v","Initializing network...")
+        return Single.just(connectivityManager.allNetworks.find {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                connectivityManager.getNetworkCapabilities(it)
+                    .hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+            } else {
+                connectivityManager.getNetworkInfo(it).extraInfo == wifiSSID
+            }
+        })
+    }
+
+    private fun addNetwork() {
+        Log.v("v","Connecting to ${wifiSSID}...")
+        val wc = WifiConfiguration()
+        wc.SSID = "\"" + wifiSSID + "\""
+        wc.preSharedKey = "\"" + wifiPassword + "\""
+        wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK)
+        val netId = wifiManager.addNetwork(wc)
+        if (netId != -1) {
+            if (!wifiManager.enableNetwork(netId, true)) {
+                Log.e("e","Failed to connect to the device.")
+            }
+        } else {
+            Log.e("e","Failed to connect to the device. addNetwork() returned -1")
+        }
+    }
+
+    private fun isConnectedToCorrectSSID(): Boolean {
+        val currentSSID = wifiManager.connectionInfo.ssid ?: return false
+        Log.v("v","Connected to $currentSSID")
+        return currentSSID == "\"${wifiSSID}\""
+    }
+}
---
+++ app/src/main/java/com/example/voicedrone/MainActivity.kt
@@ -0,0 +1,165 @@
+package com.example.voicedrone
+
+import android.Manifest
+import android.app.AlertDialog
+import android.content.*
+import android.content.pm.PackageManager
+import android.net.wifi.ScanResult
+import android.net.wifi.SupplicantState
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
+import android.net.wifi.WifiManager.SCAN_RESULTS_AVAILABLE_ACTION
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+class MainActivity : AppCompatActivity() {
+    private val PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION = 0
+    private var wifis = arrayOf<String>()
+    private var adapter : ArrayAdapter<String>? = null
+    private var internetWiFi : TextView? = null
+    private var droneWiFi : TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var manager : WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val info : WifiInfo = manager.connectionInfo
+
+        val wifiScanReceiver = object : BroadcastReceiver() {
+
+            override fun onReceive(context: Context, intent: Intent) {
+                val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+                if (success) {
+                    Log.w("Success", "yeah!")
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        // 既に許可されているか確認
+                        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                            // 許可されていなかったらリクエストする
+                            // ダイアログが表示される
+                            requestPermissions(
+                                arrayOf(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION
+                                ),
+                                PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION);
+                            return;
+                        } else {
+                            // 許可されていた場合
+                            scanWifi()
+                        }
+                    }
+
+                } else {
+                    Log.w("Failure", "Oh,my")
+                }
+            }
+        }
+
+        applicationContext.registerReceiver(wifiScanReceiver, IntentFilter(SCAN_RESULTS_AVAILABLE_ACTION))
+        val scanResult = manager.startScan()
+
+        Log.w("scanResult", scanResult.toString())
+
+        if(info.supplicantState == SupplicantState.COMPLETED) {
+            val ssid = info.ssid
+            val nowConnectingWiFi = findViewById<TextView>(R.id.nowConnectingWiFi)
+            nowConnectingWiFi.text = ssid
+            Log.w("SSID", ssid)
+        } else {
+            Log.w("SSID", "not supplied")
+        }
+
+        // リスト項目とListViewを対応付けるArrayAdapterを用意する
+        adapter = ArrayAdapter(this, R.layout.scanned_wifi)
+
+        // ListViewにArrayAdapterを設定する
+        val listView: ListView = findViewById<View>(R.id.scannedWiFi) as ListView
+        listView.adapter = adapter
+
+        internetWiFi = findViewById(R.id.internetWiFi)
+        droneWiFi = findViewById(R.id.droneWiFi)
+
+        listView.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+            AlertDialog.Builder(this)
+                .setTitle("WiFi-setting")
+                .setMessage("どちらか選択してください。")
+                .setPositiveButton("Internet-WiFi",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        internetWiFi?.text = adapter?.getItem(position)
+                    })
+                .setNegativeButton("Drone-WiFi",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        droneWiFi?.text = adapter?.getItem(position)
+                    })
+                .show()
+        }
+
+        val connectButton = findViewById<Button>(R.id.connectButton)
+        connectButton.setOnClickListener { _ ->
+            Log.w("connectionButton", "Clicked")
+            val connection = Connection(this, internetWiFi?.text.toString(), "326824658235a")
+            connection.invoke()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSIONS_REQUEST_CODE_ACCESS_COARSE_LOCATION
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            // 許可された場合
+            scanWifi()
+        } else {
+            // 許可されなかった場合
+            // 何らかの対処が必要
+        }
+    }
+
+    private fun scanWifi() {
+        var manager : WifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val array: MutableList<ScanResult> = manager.scanResults
+        Log.w("scanResults", array.toString())
+        var newWifis = arrayOf<String>()
+        for (data in array) {
+            if(!wifis.contains(data.SSID)) {
+                newWifis += data.SSID
+            }
+            Log.w("result", data.SSID)
+        }
+        for(a in wifis){
+            Log.w("wifis", a)
+        }
+        for(a in newWifis){
+            Log.w("newWifis", a)
+        }
+
+        if(newWifis.isNotEmpty()) {
+            adapter?.addAll(*newWifis)
+        }
+
+//        for(wifi in wifis) {
+//            if(!newWifis.contains(wifi)) {
+//                adapter?.remove(wifi)
+//            }
+//        }
+        adapter?.notifyDataSetChanged()
+        wifis += newWifis
+    }
+}
